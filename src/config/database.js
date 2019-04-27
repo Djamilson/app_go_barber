69f37dfce393ee42a9f62e520fa382a88356fcce
Djamilson <djamilson@gmail.com>
2019-04-27 01:01:50 -0300

    feat: configurando o banco de dados postgresql

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'postgres',
+  // dialect: 'mysql'
+  host: '172.17.0.2',
+  username: 'djamilson',
+  password: '1alves',
+  database: 'gonodemodule2',
+  operatorAliase: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
